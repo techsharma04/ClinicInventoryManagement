2fe8b2fb1658a8d3afc5303bff3a1fecec717d86
Atul Sharma <techsharma04@gmail.com>
2025-11-23 00:23:07 -0500

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -15,9 +15,9 @@ import { store } from "./app/store";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
